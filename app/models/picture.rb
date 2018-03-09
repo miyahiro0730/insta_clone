28a4f6acedcee3622500598bd66839801759aa66
hiroshi miyagi <miyahiro0730@hotmail.co.jp>
2018-03-09 22:03:45 +0900

    バリデーション追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,8 +3,8 @@ class Picture < ApplicationRecord
   has_many :favorite_users, through: :favorites, source: :user
   belongs_to :user
 
-  validates :content, presence: true
-  validates :content, length: { in: 1..140 }
+  validates :content, presence: true, length: { in: 1..140 }
+  validates :image, presence: true
 
   mount_uploader :image, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   has_many :favorite_pictures, through: :favorites, source: :picture
 
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true
   validates :password, presence: true, length: { minimum: 6 }
 end
